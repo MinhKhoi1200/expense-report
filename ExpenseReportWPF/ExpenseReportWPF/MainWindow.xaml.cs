3ece468306f717ca29a60be1e32a6f9c844b38eb
BChaSusumu <28999049+MinhKhoi1200@users.noreply.github.com>
2020-11-22 10:54:58 +0000

    Basic UI design

--- ExpenseReportWPF/ExpenseReportWPF/MainWindow.xaml.cs
+++ ExpenseReportWPF/ExpenseReportWPF/MainWindow.xaml.cs
@@ -23,6 +23,45 @@ namespace ExpenseReportWPF
         public MainWindow()
         {
             InitializeComponent();
+
+            List<ExpenseItem> expenses = new List<ExpenseItem>();
+            expenses.Add(new ExpenseItem()
+                {
+                    Date = new DateTime(2020, 11, 20),
+                    ExpenseName = "New Steam Game",
+                    ExpenseType = "Entertainment",
+                    Amount = 4.99
+                }
+            );
+
+            expenses.Add(new ExpenseItem()
+                {
+                    Date = new DateTime(2020, 11, 21),
+                    ExpenseName = "Waitrose online order",
+                    ExpenseType = "Groceries",
+                    Amount = 70.59
+                }
+            );
+
+            expenses.Add(new ExpenseItem()
+                {
+                    Date = new DateTime(2020, 11, 20),
+                    ExpenseName = "Amazon new power cord",
+                    ExpenseType = "Misc",
+                    Amount = 7.59
+                }
+            );
+
+            dgTest.ItemsSource = expenses;
+        }
+
+        public class ExpenseItem
+        {
+            public DateTime Date { get; set; }
+            public string ExpenseName { get; set; }
+            public string ExpenseType { get; set; }
+            public double Amount { get; set; }
+
         }
     }
 }
